901cb41be489f462eeb2427138cd13b7e58fbd2c
LeeEd1 Edwards <edwardsla@live.co.uk>
2024-02-22 19:51:36 +0000

    .

--- sql-orm.py
+++ sql-orm.py
@@ -10,11 +10,11 @@ db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 
-# create a class-based model for the "artist" table
+# create a class-based model for the "Artist" table
 class Artist(base):
     __tablename__ = "artist"
     artist_id = Column(Integer, primary_key=True)
-    Name = Column(String)
+    name = Column(String)
 
 
 # create a class-based model for the "Album" table
@@ -22,25 +22,25 @@ class Album(base):
     __tablename__ = "album"
     album_id = Column(Integer, primary_key=True)
     Title = Column(String)
-    artist_id = Column(Integer, ForeignKey("Artist.artist_id"))
+    artist_id = Column(Integer, ForeignKey("artist.artist_id"))
 
 
 # create a class-based model for the "Track" table
 class Track(base):
-    __tablename__ = "Track"
+    __tablename__ = "track"
     track_id = Column(Integer, primary_key=True)
     Name = Column(String)
-    album_id = Column(Integer, ForeignKey("Album.album_id"))
-    MediaTypeId = Column(Integer, ForeignKey("Album.album_id"))
+    album_id = Column(Integer, ForeignKey("album.album_id"))
+    MediaTypeId = Column(Integer, primary_key=False)
     GenreId = Column(Integer, primary_key=False)
     composer = Column(String)
     Milliseconds = Column(Integer, primary_key=False)
     Bytes = Column(Integer, primary_key=False)
     UnitPrice = Column(Float)
+    
 
-
-# instead of connecting to the database directly directly, we will ask for a session
-# create a new instance of sessionmaker,  then point to our engine (the db)
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
 # opens an actual session by calling the Session() subclass defined above
 session = Session()
@@ -48,8 +48,7 @@ session = Session()
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
 
-
-# Query 1 - select all records form the "Artist" table
-artists = session.query(Artists)
-for artist in artists:
-    print(artist.artist_id, artist.Name, sep= "  |  ")
+# Query 1 - select all records from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#   print(artist.artist_id, artist.name, sep=" | ")
